98f61bb18e67eb7c8ec3ccbe39809446acb9f932
usdtcloud <usdtcloud@gmail.com>
2022-09-23 12:13:52 +0800

    ad时光飞逝

---
+++ src/config/mysqlConfigsimple2.js
@@ -0,0 +1,10 @@
+module.exports = {
+    host : '10.190.0.3',
+    // host : '61.111.129.144',
+    port : 3306,//端口号
+    database : 'coco',//数据库名
+    user : 'coco',//数据库用户名
+    password : 'coco'//数据库密码
+    // user : 'roots',//数据库用户名
+    // password : '123456'//数据库密码
+};
