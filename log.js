e135747fbd9b6954975387f577eb1e746e2556f9
usdtcloud <usdtcloud@gmail.com>
2022-10-13 16:22:44 +0800

    first

--- log.js
+++ log.js
@@ -1,2 +1,4 @@
 let mysql = require('./src/config/mysqlConfigsimple')
 console.log(mysql)
+let redis = require('./src/config/redisConfig_3002')
+console.log(redis)
--- src/config/redisConfig_3002.js
+++ src/config/redisConfig_3002.js
@@ -1,9 +1,10 @@
+let dotenv =  require('dotenv');
+dotenv.config('.env');
 const options = {
-    host: '10.190.0.3',
-    port: 6379,
-    password: '123456',
-    db: 2,
+    host: process.env.REDIS_HOST,
+    port: process.env.REDIS_PORT,
+    password: process.env.REDIS_PWD,
+    db: process.env.REDIS_SELECT,
     detect_buffers: true // 传入buffer 返回也是buffer 否则会转换成String
 }
-
 module.exports = options
