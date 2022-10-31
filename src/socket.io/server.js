6df07c67e2fd841e36ca11db30b3712b64c03949
“usdtcloud” <usdtcloud@gmail.com>
2022-11-01 05:18:15 +0800

    socket.io

--- src/socket.io/server.js
+++ src/socket.io/server.js
@@ -1,4 +1,5 @@
 const {Server} = require("socket.io");
+const redis = require("../utils/redis/redis_3.0.2/redis");
 const socketIo = (server) => {
     const io = new Server(server,{
         pingInterval:5000,
@@ -16,7 +17,11 @@ const socketIo = (server) => {
                 if (meta.isArray() && meta.hasOwnProperty('type')){
                     switch (meta.type){
                         case 'History':
-
+                            // klineHistory:apeusdt:1m
+                            var arg = ["klineHistory:" . meta.sub.replace('@',':'),"+inf",(meta.hasOwnProperty('startTime')?meta.startTime:"-inf"), "WITHSCORES", "LIMIT", 0, (meta.hasOwnProperty('limit')?meta.limit:500)]
+                            redis.zrevrangebyscore(arg).then(res=>{
+                                socket.emit('history',JSON.stringify(res))
+                            })
                         default:
                     }
                 }
