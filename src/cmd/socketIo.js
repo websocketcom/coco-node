8012210e64fa5274b5aa9c16f4d702ddc9b3d029
“usdtcloud” <usdtcloud@gmail.com>
2022-11-01 12:42:29 +0800

    socket.io

--- src/cmd/socketIo.js
+++ src/cmd/socketIo.js
@@ -1,9 +1,9 @@
 let dotenv = require('dotenv');
 dotenv.config('.env');
 const express = require('express')
+const bodyparser = require('body-parser')
 const app     = express()
 const http    = require('http')
-
 const socketPush   = require('../socket.io/socketIo')
 const ioServer     = require('../socket.io/server')
 const socketServer = () => {
@@ -14,19 +14,24 @@ const socketServer = () => {
         socketPush.tickerSend(io)
         socketPush.klineSend(io)
     }, 1000)
+    app.use(bodyparser.json())
 
-    app.get('/', async (req, res) => {
-        await io.emit('PushAll', JSON.stringify({
-                                                    type: "",
-                                                    data: {}
-                                                }))
+    app.post('/', async (req, res) => {
+        console.log(req.body)
+        // var resData =
+        // await io.emit('PushAll', JSON.stringify({
+        //                                             type: "",
+        //                                             data: {}
+        //                                         }))
         res.json({
                      "code": 200,
                      "data": {},
                      "msg" : ""
                  })
     })
-
+    app.post('/push',async ({body}, res) => {
+        res.json(body)
+    })
     server.listen(12345, () => {
         console.log('listening on *:12345')
     })
