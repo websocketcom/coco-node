6561e0c2068ecf7917bd09849ec7ebf6d9d0f30c
usdtcloud <usdtcloud@gmail.com>
2022-11-01 13:25:09 +0800

    修复

--- src/cmd/socketIo.js
+++ src/cmd/socketIo.js
@@ -1,9 +1,9 @@
 let dotenv = require('dotenv');
 dotenv.config('.env');
-const express = require('express')
-const bodyparser = require('body-parser')
-const app     = express()
-const http    = require('http')
+const express      = require('express')
+const bodyparser   = require('body-parser')
+const app          = express()
+const http         = require('http')
 const socketPush   = require('../socket.io/socketIo')
 const ioServer     = require('../socket.io/server')
 const socketServer = () => {
@@ -18,19 +18,31 @@ const socketServer = () => {
 
     app.post('/', async (req, res) => {
         console.log(req.body)
-        // var resData =
-        // await io.emit('PushAll', JSON.stringify({
-        //                                             type: "",
-        //                                             data: {}
-        //                                         }))
         res.json({
                      "code": 200,
                      "data": {},
                      "msg" : ""
                  })
     })
-    app.post('/push',async ({body}, res) => {
-        res.json(body)
+    app.post('/push', async ({body}, res) => {
+        var {
+                event,
+                uid,
+                data
+            }   = body
+        var sio = io
+        if (event) {
+            res.send({
+                         code   : 201,
+                         message: "没有type值"
+                     })
+        }
+        if (uid) {
+            sio = io.sockets[uid]
+        }
+        if (data) {
+            sio.emit(type, data)
+        }
     })
     server.listen(12345, () => {
         console.log('listening on *:12345')
