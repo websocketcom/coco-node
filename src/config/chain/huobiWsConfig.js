a229f98f1283617b66eecb2a0e0b0fe963354072
usdtcloud <usdtcloud@gmail.com>
2022-11-03 17:02:45 +0800

    修复

--- src/config/chain/huobiWsConfig.js
+++ src/config/chain/huobiWsConfig.js
@@ -17,18 +17,10 @@ const huobiCmd = {
        "cmd" : "$symbol$@ticker",
        "params" : []
    },
-   // "depth" : {
-   //     "cmd": "$symbol$@depth@100ms",
-   //     "params":[]//type
-   // },
    "kline" : {
        "cmd" : "$symbol$@kline_$period$",
        "params" : period
    },
-   // "trade" : {
-   //     "cmd" : "$symbol$@trade",
-   //     "params":[]//type
-   // }
 }
 
 module.exports = huobiCmd
