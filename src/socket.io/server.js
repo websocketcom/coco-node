d0bd966a13ca3336673da7ec94d1b55d54fd194c
usdtcloud <usdtcloud@gmail.com>
2022-11-05 10:43:15 +0800

    啥打法上的

--- src/socket.io/server.js
+++ src/socket.io/server.js
@@ -1,8 +1,6 @@
 const {Server}    = require("socket.io");
 const redis       = require("../utils/redis/redis_3.0.2/redis");
-const period       = require("../config/chain/periodTime");
-const pako        = require("pako");
-const {btoa}      = require("buffer");
+const period      = require("../config/chain/periodTime");
 const CompressMsg = require('../utils/CompressMsg')
 
 const socketIo = (server) => {
@@ -18,33 +16,33 @@ const socketIo = (server) => {
             try {
                 let meta = JSON.parse(data.toString());
                 if (meta.hasOwnProperty('type') && meta.hasOwnProperty('sub')) {
-                    var sub = meta.sub.split('@');
-                    var max = ((parseInt((new Date()).getTime()/1000) / period[sub[1]]) - 1) * period[sub[1]];
-                    var min = "-inf";
-                    if(meta.hasOwnProperty('startTime') && meta.startTime){
-                        min = meta.startTime + period[sub[1]]
-                    }
-                    if (min == '-inf' || min <= max){
-                        switch (meta.type) {
-                            case 'History':
-                                var arg = ["klineHistory:" + meta.sub.replace('@', ':'), "+inf", (meta.hasOwnProperty('startTime') ? meta.startTime + period[sub[1]]: "-inf"), "WITHSCORES", "LIMIT", 0, (meta.hasOwnProperty('limit') ? meta.limit : 500)]
+                    switch (meta.type) {
+                        case 'History':
+                            var sub = meta.sub.split('@');
+                            var max = ((parseInt((new Date()).getTime() / 1000) / period[sub[1]]) - 1) * period[sub[1]];
+                            var min = "-inf";
+                            if (meta.hasOwnProperty('startTime') && meta.startTime) {
+                                min = meta.startTime + period[sub[1]]
+                            }
+                            console.log(socket.pid)
+                            if (min == '-inf' || min <= max) {
+                                var arg = ["klineHistory:" + meta.sub.replace('@', ':'), "+inf", (meta.hasOwnProperty('startTime') ? meta.startTime + period[sub[1]] : "-inf"), "WITHSCORES", "LIMIT", 0, (meta.hasOwnProperty('limit') ? meta.limit : 500)]
                                 redis.zrevrangebyscore(arg).then(res => {
-
                                     socket.emit('History', CompressMsg({
                                                                            cid  : sub[0],
                                                                            cycle: sub[1],
                                                                            list : res
                                                                        }))
                                 })
-                                break;
-                            case "NowList":
-                                socket.emit('NowList', CompressMsg({ }))
-                                break;
-                            case "Message":
-                                socket.emit('Message', CompressMsg({}))
-                                break;
-                            default:
-                        }
+                            }
+                            break;
+                        case "NowList":
+                            socket.emit('NowList', CompressMsg({}))
+                            break;
+                        case "Message":
+                            socket.emit('Message', CompressMsg({}))
+                            break;
+                        default:
                     }
                 }
             } catch (err) {
