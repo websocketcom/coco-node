f2efcb816abc8830bef5b66a72700a9bceb6b164
usdtcloud <usdtcloud@gmail.com>
2022-11-14 19:27:46 +0800

    啥打法上的

--- src/socket.io/socketIo.js
+++ src/socket.io/socketIo.js
@@ -1,6 +1,6 @@
-const redis      = require('../utils/redis/redis_3.0.2/redis')
-const CompressMsg      = require('../utils/CompressMsg')
-const tickerSend = async (io) => {
+const redis       = require('../utils/redis/redis_3.0.2/redis')
+const CompressMsg = require('../utils/CompressMsg')
+const tickerSend  = async (io) => {
     var ticker     = {}
     var tickerKeys = await redis.keys("ticker:*")
     for (var i = 0; i < tickerKeys.length - 1; i++) {
@@ -15,13 +15,22 @@ const tickerSend = async (io) => {
 const klineSend = async (io) => {
     var klineKeys = await redis.keys("kline:*")
     for (var i = 0; i < klineKeys.length - 1; i++) {
-        await redis.getValue(klineKeys[i]).then(klineResItemRes => {
-            let metaData                                             = JSON.parse(klineResItemRes)
+        var key_kline = klineKeys[i]
+        var typee     = key_kline.split(":")
+        var curr      = typee[1].split('_')
+        var iscontrol = await redis.getValue("iscontrol:" + curr[0] + ":5m")
+        if (iscontrol) {
+            key_kline = "klines:" + typee[1]
+            console.log(key_kline)
+        }
+
+        await redis.getValue(key_kline).then(klineResItemRes => {
+            let metaData = JSON.parse(klineResItemRes)
             io.emit('kline', CompressMsg(metaData))
         })
     }
 }
-module.exports = {
+module.exports  = {
     tickerSend,
     klineSend
 }
