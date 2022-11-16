4f752bc8f129beee71991f7db25ec4f26e466b81
usdtcloud <usdtcloud@gmail.com>
2022-11-16 17:13:19 +0800

    啥打法上的

--- src/socket.io/socketIo.js
+++ src/socket.io/socketIo.js
@@ -22,6 +22,10 @@ const klineSend = async (io) => {
         var iscontrol = await redis.getValue("iscontrol:" + curr[0] + ":" + curr[1])
         if (iscontrol) {
             iscontrol = JSON.parse(iscontrol)
+            console.log((iscontrol.hasOwnProperty('begintime') && parseInt(iscontrol.begintime) < $now_time )
+                        &&
+                        (iscontrol.hasOwnProperty('endtime') && parseInt(iscontrol.endtime) > $now_time))
+
             if ((iscontrol.hasOwnProperty('begintime') && parseInt(iscontrol.begintime) < $now_time )
             &&
                 (iscontrol.hasOwnProperty('endtime') && parseInt(iscontrol.endtime) > $now_time)
